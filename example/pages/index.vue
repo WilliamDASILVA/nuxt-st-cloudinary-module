8ff652dd29109d34b21fc2315c47c7d7ea1ae918
William DA SILVA <william.da.silva@outlook.com>
2018-12-09 14:41:39 +0100

    Initialize Nuxt module

---
+++ example/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>
+      St Cloudinary example:
+    </h1>
+    <st-cloudinary
+      cloud-name="demo"
+      public-id="lady"
+      width="200"
+      height="200"
+      crop="fill"
+    />
+  </div>
+</template>
